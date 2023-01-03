3a36e2182a7038cfd769edf944a5be532ddbd3e2
pozoqo <119871654+ozoqoproject@users.noreply.github.com>
2023-01-03 17:33:22 +0100

    Increase protocol and minimum peer proto version

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70016;
+static const int PROTOCOL_VERSION = 70017;
 static const int OLD_VERSION = 70014; // pozoqo: used to communicate with clients that don't know how to send PoS information in headers
 
 //! initial proto version, to be increased after version/verack negotiation
@@ -19,7 +19,7 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70016;
+static const int MIN_PEER_PROTO_VERSION = 70017;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
